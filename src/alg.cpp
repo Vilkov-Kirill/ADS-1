e3385012a66e76be067019ae89c630a81cb1586b
Vilkov-Kirill <99217802+Vilkov-Kirill@users.noreply.github.com>
2022-02-27 11:25:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,15 +4,15 @@
 
 
 bool checkPrime(uint64_t value) {
-    for (int i = 2; i < value / 2 + 1; i++) {
+    for (uint64_t i = 2; i < value / 2 + 1; i++) {
         if (value % i == 0) { return false; }
     }
     return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    int pr = 0;
-    for (int i = 2; i <= n; i++) {
+    uint64_t pr = 0;
+    for (uint64_t i = 2; i <= n; i++) {
         if (checkPrime(i)) { pr++; }
     }
     return pr;
@@ -27,8 +27,8 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    int s = 0;
-    for (int i = 2; i < hbound; i++) {
+    uint64_t s = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) { s += i; }
     }
     return s;
